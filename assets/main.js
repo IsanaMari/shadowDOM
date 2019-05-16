7e409ad76c44ca6aa320e0848e87e34f02de75e0
IsanaMari <jannapavl@gmail.com>
2019-05-16 05:03:18 +0300

    changes

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,27 @@ function headerNavBar() {
     })
 }
 headerNavBar()
+
+// function changeimg(link, e) {
+//     link = document.getElementById("img").src;
+//     var nodes = document.getElementById("thumb_img");
+//     var img_child = nodes.children;
+//     for (i = 0; i < img_child.length; i++) {
+//         img_child[i].classList.remove('active')
+//     }
+//     console.log('opa')
+//     e.classList.add('active');
+//
+// }
+
+function opa() {
+    let items = document.querySelectorAll('.carousel--items img'),
+        selected = document.querySelector('.carousel--selected img');
+    items.forEach(function (currentValue, index) {
+        let url = currentValue.src;
+        currentValue.addEventListener('click', function (e) {
+            selected.src = this.src;
+        })
+    })
+}
+opa()
