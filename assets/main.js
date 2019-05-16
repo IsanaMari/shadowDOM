8ef55af61fd202ed6777d96ef32beeb3c926b30a
IsanaMari <jannapavl@gmail.com>
2019-05-16 03:35:43 +0300

    change

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,9 @@
 function headerNavBar() {
-    let btn = document.querySelector('.menu_btn');
+    let btn = document.querySelector('.menu_btn'),
+        header = document.querySelector('header')
     btn.addEventListener('click', function (e) {
         this.classList.toggle('active');
+        header.classList.toggle('open')
     })
 }
 headerNavBar()
